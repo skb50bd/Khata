be2155fc142fda7e08abf5b78dbf446dc66f33ee
Shakib Haris <skb50bd@outlook.com>
2019-03-15 11:35:39 +0600

    User avatar shown on nav
    Related Work Items: #52

--- src/Khata/WebUI/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/Khata/WebUI/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -30,8 +30,6 @@ namespace WebUI.Areas.Identity.Pages.Account.Manage
             _emailSender = emailSender;
         }
 
-        public string Username { get; set; }
-
         public bool IsEmailConfirmed { get; set; }
 
         [TempData]
@@ -42,13 +40,21 @@ namespace WebUI.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            public string Username { get; set; }
+
             [Required]
             [EmailAddress]
             public string Email { get; set; }
 
-            [Phone]
-            [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
+            //[Phone]
+            //[Display(Name = "Phone number")]
+            //public string PhoneNumber { get; set; }
 
             public IFormFile Avatar { get; set; }
         }
@@ -61,16 +67,12 @@ namespace WebUI.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            var userName = await _userManager.GetUserNameAsync(user);
-            var email = await _userManager.GetEmailAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-
-            Username = userName;
-
             Input = new InputModel
             {
-                Email = email,
-                PhoneNumber = phoneNumber
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Username = user.UserName,
+                Email = user.Email
             };
 
             IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
@@ -89,6 +91,14 @@ namespace WebUI.Areas.Identity.Pages.Account.Manage
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (user.FirstName != Input.FirstName
+             || user.LastName  != Input.LastName)
+            {
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+                await _userManager.UpdateAsync(user);
             }
 
 
@@ -112,18 +122,6 @@ namespace WebUI.Areas.Identity.Pages.Account.Manage
                     throw new InvalidOperationException($"Unexpected error occurred setting email for user with ID '{userId}'.");
                 }
             }
-
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
-            {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
-                }
-            }
-
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
--- src/Khata/WebUI/Pages/Index.cshtml.cs
+++ src/Khata/WebUI/Pages/Index.cshtml.cs
@@ -1,14 +1,20 @@
-﻿using Domain;
-
+﻿using System;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
+using Brotal.Extensions;
+using Newtonsoft.Json;
 
 namespace WebUI.Pages
 {
     public class IndexModel : PageModel
     {
         public OutletOptions Options { get; set; }
-        public IndexModel(IOptionsMonitor<OutletOptions> options)
+        public IndexModel(
+            IOptionsMonitor<OutletOptions> options)
         {
             Options = options.CurrentValue;
         }
