0602828e8ea4cc00e232fbd72d00c2e8f4b101e9
Shakib Haris <skb50bd@outlook.com>
2019-03-20 21:53:44 +0600

    Updated Logger To Read From Configuration Only

--- src/Khata/Business/LoggerService.cs
+++ src/Khata/Business/LoggerService.cs
@@ -1,41 +1,17 @@
-﻿using System;
-using System.IO;
-
+﻿
 using Microsoft.Extensions.Configuration;
 
 using Serilog;
-using Serilog.Events;
 
 namespace Business
 {
     public static class LoggerService
     {
-        private static readonly IConfiguration Configuration
-            = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json",
-                    false,
-                    true)
-                .AddEnvironmentVariables()
-                .Build();
-        public static void CreateLogger()
+        public static void CreateLogger(IConfiguration conf)
         {
             Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(Configuration)
-                .MinimumLevel.Information()
-                .MinimumLevel.Override("Microsoft",
-                     LogEventLevel.Warning)
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .WriteTo.File(
-                     Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs/.log"),
-                     fileSizeLimitBytes: 5_000_000,
-                     rollingInterval: RollingInterval.Day,
-                     rollOnFileSizeLimit: true,
-                     shared: true,
-                     flushToDiskInterval: TimeSpan.FromSeconds(1),
-                     outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"
-                 ).CreateLogger();
+                        .ReadFrom.Configuration(conf)
+                        .CreateLogger();
         }
     }
 }
