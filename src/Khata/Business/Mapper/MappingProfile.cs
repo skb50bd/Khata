ff5620134f5ea8ec4b06bf7faaf02a52fc8616f7
Shakib Haris <skb50bd@outlook.com>
2019-03-26 18:11:35 +0600

    Removed Unnecessary Mapping Logic

--- src/Khata/Business/Mapper/MappingProfile.cs
+++ src/Khata/Business/Mapper/MappingProfile.cs
@@ -38,46 +38,9 @@ namespace Business.Mapper
             #endregion
 
             #region Product Mapping
-            CreateMap<Product, ProductViewModel>();
-            CreateMap<ProductViewModel, Product>()
-                .ForMember(
-                    dest => dest.Inventory,
-                    opt => opt.MapFrom(
-                        src => new Inventory
-                        {
-                            Stock = src.InventoryStock,
-                            Warehouse = src.InventoryWarehouse,
-                            AlertAt = src.InventoryAlertAt
-                        })
-                )
-                .ForMember(
-                    dest => dest.Price,
-                    opt => opt.MapFrom(
-                        src => new Pricing
-                        {
-                            Purchase = src.PricePurchase,
-                            Margin = src.PriceMargin,
-                            Bulk = src.PriceBulk,
-                            Retail = src.PriceRetail
-                        })
-                );
+            CreateMap<ProductViewModel, Product>();
 
             CreateMap<Product, ProductDto>()
-                .ForMember(
-                    dest => dest.InventoryStock,
-                    opt => opt.MapFrom(
-                        src => src.Inventory.Stock)
-                )
-                .ForMember(
-                    dest => dest.InventoryWarehouse,
-                    opt => opt.MapFrom(
-                        src => src.Inventory.Warehouse)
-                )
-                .ForMember(
-                    dest => dest.InventoryTotalStock,
-                    opt => opt.MapFrom(
-                        src => src.Inventory.TotalStock)
-                )
                 .ForMember(
                     dest => dest.InventoryStockStatus,
                     opt => opt.MapFrom(
